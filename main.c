4230f18d50e952f47ea0f8424cbd68a5458d9366
Kyungsik Cho <kscho8604@gmail.com>
2024-03-13 22:57:07 +0900

    Added mouse support.

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "lvgl/lvgl.h"
+#include "src/drivers/lv_drivers.h"
 #include "lvgl/demos/lv_demos.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <time.h>
 
 void test_mysql(void);
+void ui_init(void);
 
 int main(int argc, char **argv)
 {
@@ -15,6 +17,13 @@ int main(int argc, char **argv)
     lv_display_t * disp = lv_linux_fbdev_create();
     lv_linux_fbdev_set_file(disp, "/dev/fb0");
 
+    lv_indev_t *mouse_indev = lv_evdev_create(LV_INDEV_TYPE_POINTER, "/dev/input/event0");
+
+    LV_IMG_DECLARE(mouse_cursor_icon)
+    lv_obj_t *cursor_obj = lv_img_create(lv_scr_act());
+    lv_img_set_src(cursor_obj, &mouse_cursor_icon);
+    lv_indev_set_cursor(mouse_indev, cursor_obj);
+
 #if 0
 	if(!lv_demos_create(&argv[1], argc - 1)) {
 		lv_demos_show_help();
@@ -22,7 +31,8 @@ int main(int argc, char **argv)
 	}
 #else
 
-        test_mysql();
+       test_mysql();
+    //   ui_init();
 #endif
 	while(1) {
 		uint32_t delay = lv_timer_handler();
